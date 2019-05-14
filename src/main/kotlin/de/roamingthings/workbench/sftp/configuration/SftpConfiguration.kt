74a6192eeb85bb6ccef03fe100d058fbbb58d9ce
Alexander Sparkowsky <info@roamingthings.de>
2019-05-14 23:31:46 +0200

    Add experiements with error handling and postgres database

--- src/main/kotlin/de/roamingthings/workbench/sftp/configuration/SftpConfiguration.kt
+++ src/main/kotlin/de/roamingthings/workbench/sftp/configuration/SftpConfiguration.kt
@@ -1,26 +1,39 @@
 package de.roamingthings.workbench.sftp.configuration
 
 import com.jcraft.jsch.ChannelSftp.LsEntry
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.integration.annotation.InboundChannelAdapter
 import org.springframework.integration.annotation.Poller
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.annotation.Transformer
+import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.file.filters.CompositeFileListFilter
 import org.springframework.integration.file.filters.FileListFilter
+import org.springframework.integration.file.remote.RemoteFileTemplate
 import org.springframework.integration.file.remote.session.SessionFactory
+import org.springframework.integration.handler.advice.ExpressionEvaluatingRequestHandlerAdvice
 import org.springframework.integration.jdbc.metadata.JdbcMetadataStore
-import org.springframework.integration.metadata.ConcurrentMetadataStore
 import org.springframework.integration.sftp.filters.SftpPersistentAcceptOnceFileListFilter
 import org.springframework.integration.sftp.filters.SftpRegexPatternFileListFilter
 import org.springframework.integration.sftp.inbound.SftpStreamingMessageSource
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory
 import org.springframework.integration.sftp.session.SftpRemoteFileTemplate
+import org.springframework.integration.transaction.DefaultTransactionSynchronizationFactory
+import org.springframework.integration.transaction.ExpressionEvaluatingTransactionSynchronizationProcessor
+import org.springframework.integration.transaction.PseudoTransactionManager
+import org.springframework.integration.transaction.TransactionSynchronizationFactory
 import org.springframework.integration.transformer.StreamTransformer
 import org.springframework.messaging.MessageHandler
+import org.springframework.messaging.support.ErrorMessage
 import java.util.regex.Pattern
 import javax.sql.DataSource
 
+
+val log = getLogger("MessageHandler")
 
 @Configuration
 class SftpConfiguration(
@@ -40,7 +53,7 @@ class SftpConfiguration(
                 template(),
                 Comparator.comparing(LsEntry::getFilename)
         )
-        messageSource.setRemoteDirectory("foos")
+        messageSource.setRemoteDirectory("share/foos")
         messageSource.setFilter(filter())
         messageSource.maxFetchSize = 1
         messageSource.isCountsEnabled = true
@@ -56,7 +69,7 @@ class SftpConfiguration(
     }
 
     @Bean
-    fun template(): SftpRemoteFileTemplate {
+    fun template(): RemoteFileTemplate<LsEntry>? {
         return SftpRemoteFileTemplate(cachedSessionFactory);
     }
 
@@ -68,25 +81,94 @@ class SftpConfiguration(
     }
 
     @Bean
-    fun metadataStore(): ConcurrentMetadataStore {
+    fun metadataStore(): JdbcMetadataStore {
         return JdbcMetadataStore(dataSource)
     }
 
+    @Bean
+    fun sftpChannel() = DirectChannel()
 
     @Bean
-    @Transformer(inputChannel = "sftpChannel", outputChannel = "data")
+    fun dataChannel() = DirectChannel()
+
+    @Bean
+    @Transformer(inputChannel = "sftpChannel", outputChannel = "dataChannel")
     fun transformer(): org.springframework.integration.transformer.Transformer {
         return StreamTransformer("UTF-8")
     }
 
     @Bean
-    @ServiceActivator(inputChannel = "data")
-    fun handlerContent(store: ConcurrentMetadataStore): MessageHandler {
+    fun transactionManager() = PseudoTransactionManager()
+
+    @Bean
+    fun transactionSynchronizationFactory(applicationContext: ApplicationContext): TransactionSynchronizationFactory {
+        val parser = SpelExpressionParser()
+
+        val syncProcessor = ExpressionEvaluatingTransactionSynchronizationProcessor()
+        syncProcessor.setBeanFactory(applicationContext.autowireCapableBeanFactory);
+//    syncProcessor.setAfterCommitExpression(parser.parseExpression(
+//            "payload.renameTo(new java.io.File('test/archive' " +
+//                    " + T(java.io.File).separator + 'ARCHIVE-' + payload.name))"));
+        syncProcessor.setAfterRollbackExpression(parser.parseExpression(
+                "payload.delete()"));
+        return DefaultTransactionSynchronizationFactory(syncProcessor);
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "dataChannel", adviceChain = ["afterAdvice"])
+    fun handlerContent(store: JdbcMetadataStore): MessageHandler {
         return MessageHandler { message ->
             val remoteDirectory = message.headers["file_remoteDirectory"] as String
             val remoteFile = message.headers["file_remoteFile"] as String
-            println("Processing <$remoteDirectory/$remoteFile>")
-            println(message.payload.toString())
+            log.info("\nProcessing <$remoteDirectory/$remoteFile>\n${message.payload}")
+
+            if (message.payload.toString().contains("exception")) {
+                throw IllegalStateException("Boooom")
+            }
+        }
+    }
+
+    @Bean
+    fun afterAdvice(): ExpressionEvaluatingRequestHandlerAdvice {
+        val advice = ExpressionEvaluatingRequestHandlerAdvice()
+        advice.setSuccessChannelName("successChannel")
+        advice.setOnSuccessExpressionString(
+                "@template.remove(headers['file_remoteDirectory'] + '/' + headers['file_remoteFile'])")
+
+        advice.setFailureChannelName("failureChannel")
+//        advice.setOnFailureExpressionString(
+//                "@template.rename(headers['file_remoteDirectory'] + '/' + headers['file_remoteFile'], headers['file_remoteDirectory'] + '/' + headers['file_remoteFile'] + '.failed')")
+
+        advice.setPropagateEvaluationFailures(true)
+        // Exception will not be logged
+//        advice.setTrapException(true)
+        return advice
+    }
+
+    // https://github.com/spring-projects/spring-integration/blob/master/src/reference/asciidoc/handler-advice.adoc#expression-advice
+    @Bean
+    @ServiceActivator(inputChannel = "successChannel")
+    fun handlerSuccess(): MessageHandler {
+        return MessageHandler { message ->
+            log.info("Success!")
+        }
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "failureChannel")
+    fun handlerFailure(store: JdbcMetadataStore, sftpSessionFactory: DefaultSftpSessionFactory): MessageHandler {
+        return MessageHandler { message ->
+            log.error("Failure!")
+            if (message is ErrorMessage) {
+                val adviceException = message.payload as ExpressionEvaluatingRequestHandlerAdvice.MessageHandlingExpressionEvaluatingAdviceException
+                val failedMessage = adviceException.failedMessage
+                val remoteDirectory = failedMessage?.headers?.get("file_remoteDirectory") as String?
+                val remoteFile = failedMessage?.headers?.get("file_remoteFile") as String?
+                if (remoteFile != null && remoteDirectory != null) {
+                    sftpSessionFactory.session.rename("$remoteDirectory/$remoteFile", "$remoteDirectory/$remoteFile.failed")
+                    store.remove("streaming-$remoteFile")
+                }
+            }
         }
     }
 }
--- src/test/kotlin/de/roamingthings/workbench/sftp/SimpleSftpTest.kt
+++ src/test/kotlin/de/roamingthings/workbench/sftp/SimpleSftpTest.kt
@@ -1,11 +1,14 @@
 package de.roamingthings.workbench.sftp
 
-import de.roamingthings.workbench.sftp.configuration.SftpProperties
+import com.jcraft.jsch.ChannelSftp
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.integration.file.remote.session.SessionFactory
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.wait.strategy.Wait.forListeningPort
@@ -17,16 +20,19 @@ private const val SFTP_PORT = 22
 
 class KDockerComposeContainer(composeFiles: File) : DockerComposeContainer<KDockerComposeContainer>(composeFiles)
 
-@ActiveProfiles("test")
+@ActiveProfiles("test", "postgres")
 @Testcontainers
 @SpringBootTest
 class SimpleSftpTest {
 
     @Autowired
-    lateinit var sftpProperties: SftpProperties
+    lateinit var simpleSftpService: SimpleSftpService
 
     @Autowired
-    lateinit var simpleSftpService: SimpleSftpService
+    lateinit var sftpSessionFactory: DefaultSftpSessionFactory
+
+    @Autowired
+    lateinit var cachedSessionFactory: SessionFactory<ChannelSftp.LsEntry>
 
     companion object {
         @Container
@@ -35,18 +41,31 @@ class SimpleSftpTest {
     }
 
     @BeforeEach
-    fun setup() {
-        sftpProperties.host = sftp.getServiceHost("sftp", SFTP_PORT)
-        sftpProperties.port = sftp.getServicePort("sftp", SFTP_PORT)
+    fun setUp() {
+        sftpSessionFactory.setHost(sftp.getServiceHost("sftp", SFTP_PORT))
+        sftpSessionFactory.setPort(sftp.getServicePort("sftp", SFTP_PORT))
+
+        cachedSessionFactory.session.mkdir("share/foos")
+        for (count in 1..10) {
+            cachedSessionFactory.session.write("HelloWorld".byteInputStream(), "foos/foo$count.txt")
+        }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        cachedSessionFactory.session.remove("share/foos/foo*.txt")
+        cachedSessionFactory.session.rmdir("share/foos")
     }
 
     @Test
     fun `should list file on server`() {
+        val folderContentList = simpleSftpService.retrieveRemoteFolderList("/share/foos")
 
-        val folderContentList = simpleSftpService.retrieveRemoteFolderList("/foos")
+        // Pause to let integration do it's work
+        Thread.sleep(15_000)
 
         assertThat(folderContentList)
                 .extracting("filename")
-                .contains("shared-file.txt")
+                .contains("foo1.txt")
     }
 }
